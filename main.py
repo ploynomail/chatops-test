ef116e818b6a66f3497b43fa59f75c68c8261760
cyrus-zhang <cyrus_zhang@163.com>
2024-01-05 17:44:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+import os
+import sys
+
+from typing import List
+
+from alibabacloud_mse20190531.client import Client as mse20190531Client
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_mse20190531 import models as mse_20190531_models
+from alibabacloud_tea_util import models as util_models
+from alibabacloud_tea_util.client import Client as UtilClient
+
+
+class Sample:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_client(
+        access_key_id: str,
+        access_key_secret: str,
+    ) -> mse20190531Client:
+        """
+        使用AK&SK初始化账号Client
+        @param access_key_id:
+        @param access_key_secret:
+        @return: Client
+        @throws Exception
+        """
+        config = open_api_models.Config(
+            # 必填，您的 AccessKey ID,
+            access_key_id=access_key_id,
+            # 必填，您的 AccessKey Secret,
+            access_key_secret=access_key_secret
+        )
+        # Endpoint 请参考 https://api.aliyun.com/product/mse
+        config.endpoint = f'mse.cn-shanghai.aliyuncs.com'
+        return mse20190531Client(config)
+
+    @staticmethod
+    def main(
+        args: List[str],
+    ) -> None:
+        # 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        # 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378659.html
+        client = Sample.create_client(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+        get_nacos_config_request = mse_20190531_models.GetNacosConfigRequest(
+            instance_id='mse-cn-4xl3jyvbg02',
+            data_id='testa',
+            group='testA'
+        )
+        runtime = util_models.RuntimeOptions()
+        try:
+            # 复制代码运行请自行打印 API 的返回值
+            s = client.get_nacos_config_with_options(get_nacos_config_request, runtime)
+            print(s)
+        except Exception as error:
+            # 错误 message
+            print(error.message)
+            # 诊断地址
+            print(error.data.get("Recommend"))
+            UtilClient.assert_as_string(error.message)
+
+    @staticmethod
+    async def main_async(
+        args: List[str],
+    ) -> None:
+        # 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        # 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378659.html
+        client = Sample.create_client(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+        get_nacos_config_request = mse_20190531_models.GetNacosConfigRequest(
+            instance_id='mse-cn-4xl3jyvbg02',
+            data_id='testa',
+            group='testA'
+        )
+        runtime = util_models.RuntimeOptions()
+        try:
+            # 复制代码运行请自行打印 API 的返回值
+            await client.get_nacos_config_with_options_async(get_nacos_config_request, runtime)
+        except Exception as error:
+            # 错误 message
+            print(error.message)
+            # 诊断地址
+            print(error.data.get("Recommend"))
+            UtilClient.assert_as_string(error.message)
+
+
+if __name__ == '__main__':
+    Sample.main(sys.argv[1:])
+
+# pip install alibabacloud_mse20190531==5.2.7 --index-url http://mirrors.aliyun.com/pypi/simple/  --trusted-host mirrors.aliyun.com
